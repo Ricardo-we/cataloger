bf8ae29cd62b604389280ff8fdae3413d0ed2e62
Ricardo-we <totgamer75@gmail.com>
2022-02-01 15:42:25 -0600

    procfile added

--- OnlineCatalogs/settings.py
+++ OnlineCatalogs/settings.py
@@ -80,13 +80,23 @@ WSGI_APPLICATION = 'OnlineCatalogs.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'd9ba8fkejnrekt',
+        'USER': 'cnvovocejacjme',
+        'PASSWORD': 'f465a1acc9d56cc0f02ca4c1936b341700c8767916e0daf6bcc0bd9ca42077ea',
+        'HOST': 'ec2-50-17-255-244.compute-1.amazonaws.com',
+        'PORT': '5432'
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
---
+++ Procfile
@@ -0,0 +1,2 @@
+release: python manage.py migrate
+web: gunicorn ProyectoEspecialPro.wsgi --log-file=-
