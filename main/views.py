76b98c9c6b146ef0424f91809a2e87bea784046d
Ricardo-we <totgamer75@gmail.com>
2023-01-03 18:48:00 -0600

    Fixed errors

--- main/views.py
+++ main/views.py
@@ -11,17 +11,18 @@ def login(request):
 
 
 def check_user(request):
-    if(request.method == 'POST'):
-        username = request.POST.get('username')
-        email = request.POST.get('email')
-        password = request.POST.get('password')
+    try:
+        if(request.method == 'POST'):
+            username = request.POST.get('username')
+            email = request.POST.get('email')
+            password = request.POST.get('password')
 
-        if(Users.objects.filter(username=username, password=password, email=email).exists()):
-            return redirect(f'/home/?username={username}')
-        else:
-            return render(request, 'main/login/login.html', {'failed': 'true'})
-
-    return redirect('/login')
+            if(Users.objects.filter(username=username, password=password, email=email).exists()):
+                return redirect(f'/home/?username={username}')
+            else:
+                return render(request, 'main/login/login.html', {'failed': 'true'})
+    except: 
+        return redirect('/login')
 
 
 def manage_catalog_products(request, catalogid):
